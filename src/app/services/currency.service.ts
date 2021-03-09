73853be8b8eacfa128ed3af148403879ef863c68
Esubalew Alemayehu Demissie <nattiy535@gmail.com>
2021-03-09 18:11:17 +0100

    Update currency.service.ts

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Currency } from '../models/currency.model';
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/catch';
 import { map } from "rxjs/operators";
 
 const baseUrl = 'http://localhost:8080/api/currencies';
